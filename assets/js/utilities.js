94e50812f5b3071623508addcbf5fcbde267f611
Jason <jasonmcgrath123@gmail.com>
2022-01-28 14:16:19 -0500

    first commit

---
+++ assets/js/utilities.js
@@ -0,0 +1,17 @@
+  // Href click listeners
+  function customHREF(url) {
+    window.open(url, '_top');
+  }
+
+  function customHREF2(url){
+    window.open(url, '_blank');
+  }
+
+  function scrollToTop() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 1500, 'easeInOutExpo');
+    return false;
+  }
+
+  
